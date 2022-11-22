b773ba9f085bcb992befac391f39f101439d8ab7
devheyrin <beforerin@gmail.com>
2022-11-22 04:47:22 +0000

    Update main.js

---
+++ test.js
@@ -0,0 +1,28 @@
+const json = [
+  {
+    title: '범죄도시2',
+    opening_date: '2022-05-18'
+  },
+  {
+    title: '탑건:매버릭',
+    opening_date: '2022-06-22'
+  },
+  {
+    title: '한산: 용의 출현',
+    opening_date: '2022-07-27'
+  },
+  {
+    title: '공조2: 인터내셔날',
+    opening_date: '2022-09-07'
+  },
+  {
+    title: '헌트',
+    opening_date: '2022-08-10'
+  }
+]
+
+json.forEach((item) => {
+	console.log(item);
+	console.log(item.title);
+	console.log(item.opening_date);
+})
